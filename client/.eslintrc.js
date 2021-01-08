1e8aaa81542da0d4fcb153d7006b58d1b007326e
Furkan <furkvnclk@gmail.com>
2021-01-08 11:06:37 +0300

    configure eslint rules

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -25,5 +25,11 @@ module.exports = {
         'generator-star-spacing': 'off',
         // allow debugger during development
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        // disable new line requires
+        "eol-last": 0,
+        // disable indentation rule
+        "indent": "off",
+        //disable rule of space before function parentheses 
+        "space-before-function-paren": 0
     }
 }
